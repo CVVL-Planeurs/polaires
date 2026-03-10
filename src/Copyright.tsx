7aac257ae2bf404567ff7f0ea581bcf7837a2210
bruno fleisch <bruno.fleisch@bt.com>
2026-03-10 08:49:23 +0100

    ajout de l'option base; ajout lien github

--- src/Copyright.tsx
+++ src/Copyright.tsx
@@ -5,7 +5,10 @@ import { CopyrightSharp } from '@mui/icons-material';
 export function Copyright() {
 
     return (
-        <Typography className='copyright'> <CopyrightSharp fontSize='small'/>2025 Bruno Fleisch</Typography>
+        <Typography className='copyright'> <CopyrightSharp fontSize='small'/>2025 Bruno Fleisch (source: 
+        <a href="https://github.com/CVVL-Planeurs/polaires">https://github.com/CVVL-Planeurs/polaires</a>)
+        
+        </Typography>
 
     )
 }
